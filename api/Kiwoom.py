c06214339f8ff6a341792bdc0a605f04c251fafc
ByoungwooYoon <deity719@naver.com>
2023-01-15 23:02:57 +0900

    initial commit

---
+++ api/Kiwoom.py
@@ -0,0 +1,10 @@
+from PyQt5.QAxContainer import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+
+class Kiwoom(QAxWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        
+    
